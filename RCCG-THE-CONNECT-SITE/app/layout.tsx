a83855cd134cb98ee35989cecbff307a164c97b5
David Ojeata <davidojeata0@gmail.com>
2026-09-02 15:24:59 +0100

    Update layout.tsx

--- RCCG-THE-CONNECT-SITE/app/layout.tsx
+++ RCCG-THE-CONNECT-SITE/app/layout.tsx
@@ -1,23 +1,42 @@
-import type { Metadata } from "next";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 
-export const metadata: Metadata = {
-  title: "The Connect Church",
-  description: "RCCG The Connect Church",
+export const metadata = {
+  title: "Church Website",
+  description: "Modern Church Website",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
+
+      <head>
+
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+        />
+
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
+
+      </head>
+
       <body>
-        <Navbar />
         {children}
       </body>
+
     </html>
   );
 }
