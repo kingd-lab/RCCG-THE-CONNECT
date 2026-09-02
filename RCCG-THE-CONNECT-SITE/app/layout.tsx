a4b8c8a73cae9e42750d05da98d41b23fe61b298
David Ojeata <davidojeata0@gmail.com>
2026-09-02 16:26:05 +0100

    Update layout.tsx

--- RCCG-THE-CONNECT-SITE/app/layout.tsx
+++ RCCG-THE-CONNECT-SITE/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "RCCG THE CONNECT",
@@ -12,31 +13,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-
       <head>
-
         <link
           rel="preconnect"
           href="https://fonts.googleapis.com"
         />
-
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin=""
         />
-
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
-
       </head>
-
       <body>
-        {children}
+        <Navbar />
+        <main className="pt-20">{children}</main>
       </body>
-
     </html>
   );
 }
