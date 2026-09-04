1d6af9d59164f019557de36556e8b4c4ee39cb90
David Ojeata <davidojeata0@gmail.com>
2026-09-04 11:31:38 +0100

    Update layout.tsx

--- RCCG-THE-CONNECT-SITE/app/layout.tsx
+++ RCCG-THE-CONNECT-SITE/app/layout.tsx
@@ -4,6 +4,12 @@ import Navbar from "@/components/Navbar";
 export const metadata = {
   title: "RCCG THE CONNECT",
   description: "The Connect Church Website",
+
+  icons: {
+    icon: "/logo.png",
+    shortcut: "/logo.png",
+    apple: "/logo.png",
+  },
 };
 
 export default function RootLayout({
@@ -18,16 +24,19 @@ export default function RootLayout({
           rel="preconnect"
           href="https://fonts.googleapis.com"
         />
+
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin=""
         />
+
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
       </head>
+
       <body>
         <Navbar />
         {children}
